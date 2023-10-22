a4c2197f271b51291198faaeeda41ce4891c61a0
Izofeu <60717460+Izofeu@users.noreply.github.com>
2023-10-22 11:09:56 +0200

    Reorder cookies.php
    Now the user will be able to reauthenticate if an auth password gets changed (bugfix)
    Minor CSS change

--- scripts/cookies.php
+++ scripts/cookies.php
@@ -11,21 +11,22 @@
 	$authfile = fopen("scripts/auth.pwd", "r") or die ("Cannot open password file.");
 	$auth = fread($authfile, filesize("scripts/auth.pwd"));
 	fclose($authfile);
-	if(isset($_COOKIE["auth"]))
-	{
-		if($_COOKIE["auth"] == $auth)
-		{
-			$isauth = true;
-			setcookie("auth", $auth, time() + (86400 * 7), "/", $domain, false, true);
-		}
-	}
-	else if(isset($_POST["auth"]))
+	
+	if(isset($_POST["auth"]))
 	{
 		$authsuccessful = false;
 		if($_POST["auth"] == $auth)
 		{
 			$isauth = true;
 			$authsuccessful = true;
+			setcookie("auth", $auth, time() + (86400 * 7), "/", $domain, false, true);
+		}
+	}
+	else if(isset($_COOKIE["auth"]))
+	{
+		if($_COOKIE["auth"] == $auth)
+		{
+			$isauth = true;
 			setcookie("auth", $auth, time() + (86400 * 7), "/", $domain, false, true);
 		}
 	}
